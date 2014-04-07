92ce76f229f9d0f07c155bee5e107d6e69b2e9b4
Alex's Chromebook <alexmckenley@gmail.com>
2014-04-06 21:31:59 -0700

    implemented tab system

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
+(function(){
+  var nav = document.getElementsByTagName('nav')[0];
+  nav.addEventListener('click', function(e){
+    if(e.target.tagName === 'A'){
+      var allContent = document.querySelectorAll('.main-content div');
+      for(var i = 0; i < allContent.length; i++){
+        allContent[i].style.display = 'none';
+      }
+      var className  = e.target.className;
+      var content = document.querySelector('.main-content .' + className);
+      content.style.display = "block";
+    }
+  });
 
-
-
-});
+})();
