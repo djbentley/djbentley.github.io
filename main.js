5a127e236cf532ac32d1cb3b4dfb13649d54b9ef
Alex's Chromebook <alexmckenley@gmail.com>
2014-04-06 22:09:58 -0700

    updated contact info

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 (function(){
   var nav = document.getElementsByTagName('nav')[0];
   nav.addEventListener('click', function(e){
+    var i;
     if(e.target.tagName === 'A'){
-      var allContent = document.querySelectorAll('.main-content div');
-      for(var i = 0; i < allContent.length; i++){
+      var allLinks= document.querySelectorAll('nav a');
+      for(i = 0; i < allLinks.length; i++){
+        allLinks[i].style.color = 'white';
+      }
+      var allContent = document.querySelectorAll('.main-content > div');
+      for(i = 0; i < allContent.length; i++){
         allContent[i].style.display = 'none';
       }
+
       var className  = e.target.className;
       var content = document.querySelector('.main-content .' + className);
       content.style.display = "block";
+      e.target.style.color = "#FFD200";
     }
   });
 
